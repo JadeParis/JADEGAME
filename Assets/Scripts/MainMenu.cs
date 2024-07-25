3dfca5525aaef7ce10d478296e8d99df08842c83
JadeParis <jadeparis04@gmail.com>
2024-07-25 12:21:45 +1200

    UI stufffffff!

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class MainMenu : MonoBehaviour
+{
+  public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Controls()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void BackTitle()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+}
